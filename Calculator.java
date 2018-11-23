6560014c40ec383804204eed1bdde7c734c8ee2b
YoungTae0406 <201802073@o.cnu.ac.kr>
2018-11-23 19:55:35 +0900

    check test

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+  public static void main(String[] args){
+    int a= 4;
+    int b= 2;
+    System.out.println(sum(a,b));
+  }
+  public static int sum(int a, int b){
+    return a+b;
+  }
+  jfsaj
+}
